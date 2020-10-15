5e1880d7b9c3c78b38b75b671f2edb8d728ab975
eunseo2 <70589857+eunseo2@users.noreply.github.com>
2020-10-15 14:50:59 +0900

    Update dept.php

--- dept.php
+++ dept.php
@@ -46,7 +46,7 @@
 
 <body>
     <h2><a href="index.php">직원 관리 시스템</a> | 부서 정보</h2>
-    <table border="1">
+    <table  border = "1" width = "300" height = "130" align = "center">
         <tr>
             <th>emp_no</th>
             <th>first_name</th>
