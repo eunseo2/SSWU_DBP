d9605940fb18985a183808c66605fdabd6ebdbf2
eunseo2 <70589857+eunseo2@users.noreply.github.com>
2020-10-15 14:13:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,14 @@
         <input type="text" name="number" placeholder="number">
         <input type="submit" value="Search">
     </form>
+    <form action="salary_info.php" method="GET">
+        (5) 부서 정보: 
+        <input type="text" name="emp_no" placeholder="emp_no">
+        <input type="submit" value="Search">
+    </form>
+    
+    
+    
     
     
 </body>
