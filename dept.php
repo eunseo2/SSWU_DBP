48ee66cbd89ee0aed55d378d4e33d1deb7490207
eunseo2 <70589857+eunseo2@users.noreply.github.com>
2020-10-15 14:23:26 +0900

    Update dept.php

--- dept.php
+++ dept.php
@@ -1,1 +1,71 @@
+<?php
+    $link = mysqli_connect('localhost', 'admin', 'admin', 'employees');
 
+    if(mysqli_connect_errno()){
+        echo "Failed to connect to MariaDB: " . mysqli_connect_error();
+        exit();
+    }    
+
+    settype($_GET['number'], 'integer');
+    $filtered_number = mysqli_real_escape_string($link, $_GET['number']);
+    
+    $query = "
+       select e.emp_no, e.first_name, d.dept_name
+  from dept_emp de
+  inner join employees e on e.emp_no=de.emp_no
+  inner join departments d on d.dept_no=de.dept_no
+  where de.to_date=’9999-01-01’
+    ";
+
+    $result = mysqli_query($link, $query);  
+    
+    $article = '';    
+    while($row = mysqli_fetch_array($result)){
+         $article .= '<tr><td>';
+        $article .= $row["emp_no"];
+        $article .= '</td><td>';
+        $article .= $row["first_name"];
+        $article .= '</td><td>';
+        $article .= $row["dept_name"];
+        $article .= '</td><td>';
+
+    }
+    
+    mysqli_free_result($result);
+    mysqli_close($link);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>부서 정보</title>
+    <style>
+        body {
+            font-family: Consolas, monospace;
+            font-family: 12px;
+        }
+        table {
+            width: 100%;
+        }
+        th, td {
+            padding: 10px;
+            border-bottom: 1px solid #dadada;
+        }
+    </style>
+</head>
+
+<body>
+    <h2><a href="index.php">직원 관리 시스템</a> | 부서 정보</h2>
+    <table>
+        <tr>
+            <th>first_name</th>
+            <th>last_name</th>
+            <th>dept_name</th>
+        </tr>        
+        <?= $article ?>
+    </table>
+</body>
+
+</html>
